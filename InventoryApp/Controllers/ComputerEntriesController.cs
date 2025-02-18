39407db03d8e5136c6f79e9b507a52afcf92138e
FitoFire <68967952+fitofire@users.noreply.github.com>
2025-02-18 14:00:39 -0800

    completed the main program

--- InventoryApp/Controllers/ComputerEntriesController.cs
+++ InventoryApp/Controllers/ComputerEntriesController.cs
@@ -56,5 +56,46 @@ namespace InventoryApp.Controllers
             }
             return View(computerEntry);
         }
+
+        [HttpPost]
+        public IActionResult Edit(ComputerEntry computerEntry)
+        {
+            if (computerEntry != null && computerEntry.SerialName.Length < 5)
+            {
+                ModelState.AddModelError("SerialName", "Serial Name must be at least 5 characters long.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.ComputerEntries.Update(computerEntry);  // Update the computer entry in the database
+                _db.SaveChanges();           // Save changes to the database
+                return RedirectToAction("Index");
+            }
+            return View(computerEntry);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            ComputerEntry? computerEntry = _db.ComputerEntries.Find(id);
+
+            if (computerEntry == null)
+            {
+                return NotFound();
+            }
+            return View(computerEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(ComputerEntry computerEntry)
+        {
+            _db.ComputerEntries.Remove(computerEntry);  // Remove the computer entry in the database
+            _db.SaveChanges();           // Save changes to the database
+            return RedirectToAction("Index");
+        }
     }
 }
